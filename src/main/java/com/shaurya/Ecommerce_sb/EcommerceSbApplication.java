9aabdb83612693b0433974d6cdc30cd33a847bab
Shaurya <shauryab22@gmail.com>
2026-01-13 12:52:27 +0530

    Initial Commit

---
+++ src/main/java/com/shaurya/Ecommerce_sb/EcommerceSbApplication.java
@@ -0,0 +1,13 @@
+package com.shaurya.Ecommerce_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSbApplication.class, args);
+	}
+
+}
